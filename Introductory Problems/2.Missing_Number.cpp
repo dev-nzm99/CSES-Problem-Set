1b04cb14311a579640353650f571c0450e0335bb
nazmulislam <nazmulislam93400@gmail.com>
2025-10-02 08:21:47 +0600

    Update 2.Missing_Number.cpp

--- Introductory Problems/2.Missing_Number.cpp
+++ Introductory Problems/2.Missing_Number.cpp
@@ -1,1 +1,13 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main(){
+    ll n,total,sum;cin>>n;
+    total = (n*(n+1))/2;n--;
+    sum = 0;
+    while(n--){
+        ll a;cin>>a;
+        sum += a; 
+    }cout<<(total-sum)<<endl;     
+    return 0;
+}
