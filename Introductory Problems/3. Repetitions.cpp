4e7245a33b05563345c3c162a29d9103cd1d46f0
nazmulislam <nazmulislam93400@gmail.com>
2025-10-02 09:03:52 +0600

    Update 3. Repetitions.cpp

--- Introductory Problems/3. Repetitions.cpp
+++ Introductory Problems/3. Repetitions.cpp
@@ -1,1 +1,13 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;cin>>str;
+    int n = str.size();
+    int max_freq,curr_freq;
+    max_freq = curr_freq = 1;
+    for(int i = 0;i<n-1;i++){
+        (str[i]==str[i+1])?curr_freq++:curr_freq=1;
+        max_freq = max(max_freq,curr_freq);
+    }cout<<max_freq<<endl;
+    return 0;
+}
