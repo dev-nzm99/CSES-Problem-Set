deee48a3e7191d8d11467cbf1de988d9bd2a3ac9
nazmulislam <nazmulislam93400@gmail.com>
2025-10-02 08:12:43 +0600

    Update 1.Weird_Algorithm.cpp

--- Introductory Problems/1.Weird_Algorithm.cpp
+++ Introductory Problems/1.Weird_Algorithm.cpp
@@ -1,1 +1,12 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main(){
+    ll n;cin>>n;
+    cout<<n<<" ";
+    while(n>1){
+        (n&1)?n=n*3+1:n/=2;
+        cout<<n<<" ";
+    }cout<<endl;     
+    return 0;
+}
