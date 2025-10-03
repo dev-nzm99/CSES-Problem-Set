2f9d0aaa102243bd51e071b04b7537340e92f803
nazmulislam <nazmulislam93400@gmail.com>
2025-10-03 23:37:07 +0600

    Update 4. Increasing_Array.cpp

--- Introductory Problems/4. Increasing_Array.cpp
+++ Introductory Problems/4. Increasing_Array.cpp
@@ -1,1 +1,16 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long int;
+int main(){
+    int n;cin>>n;
+    vector<ll> a(n);
+    for(auto &x:a)cin>>x;
+    ll no_of_moves = 0;
+    for(int i = 1;i<n;i++){
+        if(a[i]>=a[i-1])continue;
+        no_of_moves += abs(a[i]-a[i-1]);
+        a[i] = a[i-1];
+    }
+    cout<<no_of_moves<<endl;
+    return 0; 
+}
